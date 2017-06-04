9b521f29b2f0f3273db9a5edbee0c18b08f45a6f
VincentHwuang <Vincent_Hwuang@hotmail.com>
2017-06-04 14:40:21 +0800

    the first commit

---
+++ tutorial/tutorial/spiders/quotes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+	name = 'quotes'
+#    allowed_domains = ['http://quotes.toscrape.com/page/1']
+#    start_urls = ['http://http://quotes.toscrape.com/page/1/']
+	def start_requests(self):
+		urls=[
+		'http://quotes.toscrape.com/page/1',
+		'http://quotes.toscrape.com/page/2'
+		]
+		for url in urls:
+			yield scrapy.Request(url=url,callback=self.parse)
+
+    	def parse(self, response):
+		for quote in response.css('div.quote'):
+			yield {
+			'text'  :  quote.css('span.text::text').extract_first(),
+			'author':  quote.css('small.author::text').extract_first(),
+			'tags'  :  quote.css('div.tags a.tag::text').extract(),
+			}
