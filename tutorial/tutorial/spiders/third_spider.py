4e8d781d41ebd9a4383c327404a1942b900422ad
VincentHwuang <Vincent_Hwuang@hotmail.com>
2017-06-05 19:07:20 +0800

    committed at 19:07 Mon June 5,2017

--- tutorial/tutorial/spiders/third_spider.py
+++ tutorial/tutorial/spiders/third_spider.py
@@ -11,11 +11,11 @@ class ThirdQuotesSpider(scrapy.Spider):
 			yield {
 			'text':quote.css('span.text::text').extract_first(),
 			'author':quote.css('small.author::text').extract_first(),
-			'tags':quote.css('div.tags a.tag::text')extract(),
+			'tags':quote.css('div.tags a.tag::text').extract(),
 			}
 
-	next_page=response.css('li.next a::attr(href)').extract_first()
-	if next_page in not None:
-		next_page=response.urljoin(next_page)
-		yield scrapy.Request(url=next_page,callback=self.parse)
+		next_page=response.css('li.next a::attr(href)').extract_first()
+		if next_page is not None:
+			next_page=response.urljoin(next_page)
+			yield scrapy.Request(url=next_page,callback=self.parse)
 
